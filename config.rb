fc7e0ec58e138bda5e2fbc8108f4f855524582e1
Javier usobiaga <javier@swwweet.com>
2017-10-08 18:31:36 +0200

    Inital commit

---
+++ config.rb
@@ -0,0 +1,12 @@
+# Activate multi-language
+
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+
+# activate :directory_indexes
+
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
